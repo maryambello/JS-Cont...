3b6f7809f0ffb4a489d5d199ebe139def6df4db6
Maryam Bello <maryambamixe@gmail.com>
2024-12-30 12:14:44 -0800

    second

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const h1 = document.querySelector("h1")
-alert("hey")
+// linking a javaScript File
+//let js = "amaizing";
+//console.log(30 + 20 + 40 - 10);
+
+// Data Types
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
