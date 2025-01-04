2b48ee205e5d5fa32d103b63b585bae3174f1148
Maryam Bello <maryambamixe@gmail.com>
2025-01-04 11:56:59 -0800

    next

---
+++ fundemental2/script.js
@@ -0,0 +1,18 @@
+// function logger() {
+//   console.log("My name is maryam");
+// }
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
