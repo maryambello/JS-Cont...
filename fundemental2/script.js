35ed93ddfa8ad623bb4293673d2158a511a5dcab
Maryam Bello <maryambamixe@gmail.com>
2025-01-07 20:16:10 -0800

    next

--- fundemental2/script.js
+++ fundemental2/script.js
@@ -16,3 +16,18 @@
 // const appleOrangeJuice = fruitProcessor(2, 4);
 // console.log(appleOrangeJuice);
 
+// funtion expression
+// const calcAge2 = function (birthYeah) {
+//   return 2037 - birthYeah;
+// };
+
+// Arrow function
+const calcAge3 = (birthYeah) => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  return retirement;
+};
